419ea0b48166603d675a89a0ba32bede5d31701d
Sebastián Ramírez <tiangolo@gmail.com>
2020-10-03 17:34:10 +0200

    🔊 Add first simple version, just debugging

---
+++ app/main.py
@@ -0,0 +1,43 @@
+import logging
+from pathlib import Path
+from typing import Optional
+
+from devtools import debug
+from github import Github
+from github.NamedUser import NamedUser
+from pydantic import BaseSettings, SecretStr
+
+from app.model import Event, Organization, PullRequest, Repository
+
+
+class Settings(BaseSettings):
+    github_repository: str
+    input_token: SecretStr
+    github_event_path: Path
+    github_event_name: Optional[str] = None
+
+
+class GitHubEventPullRequest(Event):
+    action: str
+    number: int
+    changes: Optional[dict] = None
+    pull_request: PullRequest
+    repository: Repository
+    organization: Optional[Organization] = None
+    installation: Optional[dict] = None
+    sender: Optional[dict] = None
+
+
+if __name__ == "__main__":
+    logging.basicConfig(level=logging.INFO)
+    settings = Settings()
+    logging.info(f"Using config: {settings.json()}")
+    g = Github(settings.input_token.get_secret_value())
+    repo = g.get_repo(settings.github_repository)
+    owner: NamedUser = repo.owner
+    github_event: Optional[GitHubEventPullRequest] = None
+    if settings.github_event_path.is_file():
+        contents = settings.github_event_path.read_text()
+        github_event = GitHubEventPullRequest.parse_raw(contents)
+        debug(github_event)
+    logging.info("Finished")
