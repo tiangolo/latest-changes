a2d1423181e06d34712650ed1e0a8ad58a48bd85
Sebastián Ramírez <tiangolo@gmail.com>
2020-10-10 13:54:23 +0200

    ✨ Add support for Jinja2 templates for changes notes (#23)
    * ✨ Add support for Jinja2 templates for the message
    
    * ➕ Add Jinja2 to requirements

--- app/__main__.py
+++ app/__main__.py
@@ -10,6 +10,8 @@ from github import Github
 from github.NamedUser import NamedUser
 from pydantic import BaseSettings, SecretStr
 from pydantic.main import BaseModel
+from jinja2 import Template
+
 
 from app.model import Organization, PullRequest, Repository
 
@@ -21,6 +23,7 @@ class Settings(BaseSettings):
     github_event_name: Optional[str] = None
     input_latest_changes_file: Path = Path("README.md")
     input_latest_changes_header: str = "### Latest Changes\n\n"
+    input_template_file: Path = Path(__file__).parent / "latest-changes.jinja2"
 
 
 class GitHubEventPullRequest(BaseModel):
@@ -62,7 +65,9 @@ if settings.github_event_path.is_file():
     subprocess.run(
         ["git", "config", "user.email", "github-actions@github.com"], check=True
     )
-    logging.info(f"Pulling the latest changes, including the latest merged PR (this one)")
+    logging.info(
+        "Pulling the latest changes, including the latest merged PR (this one)"
+    )
     subprocess.run(["git", "pull"], check=True)
     content = settings.input_latest_changes_file.read_text()
     match = re.search(settings.input_latest_changes_header, content)
@@ -73,7 +78,9 @@ if settings.github_event_path.is_file():
         sys.exit(1)
     pre_content = content[: match.end()]
     post_content = content[match.end() :]
-    message = f"* {github_event.pull_request.title}. PR [#{github_event.pull_request.number}]({github_event.pull_request.html_url}) by [@{github_event.pull_request.user.login}]({github_event.pull_request.user.html_url}).\n"
+    template_content = settings.input_template_file.read_text("utf-8")
+    template = Template(template_content)
+    message = template.render(github_event=github_event)
     new_content = pre_content + message + post_content
     settings.input_latest_changes_file.write_text(new_content)
     logging.info(f"Committing changes to: {settings.input_latest_changes_file}")
