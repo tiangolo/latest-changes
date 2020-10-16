042d2a6ad4a176c6f60944d1586db78461992b99
Sebastián Ramírez <tiangolo@gmail.com>
2020-10-16 19:32:18 +0200

    ✨ Refactor and add support for triggering with workflow dispatch events (#32)
    * ✨ Support worfkow_dispatch, only getting a PR number to run and not an event file
    
    * ♻️ Make GitHub token required and add optional PR number for workflow_dispatch
    
    * 🔧 Add workflow_dispatch to itself
    
    * 📝 Update README with custom config

--- app/__main__.py
+++ app/__main__.py
@@ -20,7 +20,8 @@ class Settings(BaseSettings):
     github_repository: str
     github_event_path: Path
     github_event_name: Optional[str] = None
-    input_token: Optional[SecretStr] = None
+    input_token: SecretStr
+    input_number: Optional[int] = None
     input_latest_changes_file: Path = Path("README.md")
     input_latest_changes_header: str = "### Latest Changes\n\n"
     input_template_file: Path = Path(__file__).parent / "latest-changes.jinja2"
@@ -42,55 +43,59 @@ logging.basicConfig(level=logging.INFO)
 settings = Settings()
 if settings.input_debug_logs:
     logging.info(f"Using config: {settings.json()}")
-# TODO: This might be useful later for parsing a comment in the PR
-# g = Github(settings.input_token.get_secret_value())
-# repo = g.get_repo(settings.github_repository)
-# owner: NamedUser = repo.owner
-# github_event: Optional[GitHubEventPullRequest] = None
-if settings.github_event_path.is_file():
+g = Github(settings.input_token.get_secret_value())
+repo = g.get_repo(settings.github_repository)
+event: Optional[GitHubEventPullRequest] = None
+number: Optional[int] = settings.input_number
+if settings.input_number is None and settings.github_event_path.is_file():
     contents = settings.github_event_path.read_text()
-    github_event = GitHubEventPullRequest.parse_raw(contents)
-    if settings.input_debug_logs:
-        logging.info("GitHub Event object:")
-        debug(github_event)
-        logging.info(f"GitHub Event JSON: {github_event.json(indent=2)}")
-    if not github_event.pull_request.merged:
-        logging.error(
-            "The PR was not merged but this action was run, add a step to your GitHub Action with:"
-        )
-        logging.error("if: github.event.pull_request.merged == true")
-        sys.exit(1)
-    if not settings.input_latest_changes_file.is_file():
-        logging.error(
-            f"The latest changes files doesn't seem to exist: {settings.input_latest_changes_file}"
-        )
-        sys.exit(1)
-    logging.info("Setting up GitHub Actions git user")
-    subprocess.run(["git", "config", "user.name", "github-actions"], check=True)
-    subprocess.run(
-        ["git", "config", "user.email", "github-actions@github.com"], check=True
+    event = GitHubEventPullRequest.parse_raw(contents)
+    number = event.pull_request.number
+if number is None:
+    logging.error("This GitHub action must be triggered by a merged PR or with a PR number")
+    sys.exit(1)
+assert number is not None, "The PR number should exist by now"
+pr = repo.get_pull(number)
+if settings.input_debug_logs:
+    logging.info("PR object:")
+    debug(pr)
+if not pr.merged:
+    logging.error(
+        "The PR was not merged but this action was run, add a step to your GitHub Action with:"
     )
-    logging.info(
-        "Pulling the latest changes, including the latest merged PR (this one)"
+    logging.error("if: github.event.pull_request.merged == true")
+    sys.exit(1)
+if not settings.input_latest_changes_file.is_file():
+    logging.error(
+        f"The latest changes files doesn't seem to exist: {settings.input_latest_changes_file}"
     )
-    subprocess.run(["git", "pull"], check=True)
-    content = settings.input_latest_changes_file.read_text()
-    match = re.search(settings.input_latest_changes_header, content)
-    if not match:
-        logging.error(
-            f"The latest changes file at: {settings.input_latest_changes_file} doesn't seem to contain the header RegEx: {settings.input_latest_changes_header}"
-        )
-        sys.exit(1)
-    pre_content = content[: match.end()]
-    post_content = content[match.end() :]
-    template_content = settings.input_template_file.read_text("utf-8")
-    template = Template(template_content)
-    message = template.render(github_event=github_event)
-    new_content = pre_content + message + post_content
-    settings.input_latest_changes_file.write_text(new_content)
-    logging.info(f"Committing changes to: {settings.input_latest_changes_file}")
-    subprocess.run(["git", "add", str(settings.input_latest_changes_file)], check=True)
-    subprocess.run(["git", "commit", "-m", "📝 Update release notes"], check=True)
-    logging.info(f"Pushing changes: {settings.input_latest_changes_file}")
-    subprocess.run(["git", "push"], check=True)
+    sys.exit(1)
+logging.info("Setting up GitHub Actions git user")
+subprocess.run(["git", "config", "user.name", "github-actions"], check=True)
+subprocess.run(
+    ["git", "config", "user.email", "github-actions@github.com"], check=True
+)
+logging.info(
+    "Pulling the latest changes, including the latest merged PR (this one)"
+)
+subprocess.run(["git", "pull"], check=True)
+content = settings.input_latest_changes_file.read_text()
+match = re.search(settings.input_latest_changes_header, content)
+if not match:
+    logging.error(
+        f"The latest changes file at: {settings.input_latest_changes_file} doesn't seem to contain the header RegEx: {settings.input_latest_changes_header}"
+    )
+    sys.exit(1)
+pre_content = content[: match.end()]
+post_content = content[match.end() :]
+template_content = settings.input_template_file.read_text("utf-8")
+template = Template(template_content)
+message = template.render(pr=pr)
+new_content = pre_content + message + post_content
+settings.input_latest_changes_file.write_text(new_content)
+logging.info(f"Committing changes to: {settings.input_latest_changes_file}")
+subprocess.run(["git", "add", str(settings.input_latest_changes_file)], check=True)
+subprocess.run(["git", "commit", "-m", "📝 Update release notes"], check=True)
+logging.info(f"Pushing changes: {settings.input_latest_changes_file}")
+subprocess.run(["git", "push"], check=True)
 logging.info("Finished")
