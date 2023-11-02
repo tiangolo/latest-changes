f94bd935336fdbd30310250d5298b6f73c212e37
Sebastián Ramírez <tiangolo@gmail.com>
2023-11-02 19:41:19 +0400

    ♻️ Update name of end_regex config

--- latest_changes/main.py
+++ latest_changes/main.py
@@ -24,8 +24,8 @@ class Settings(BaseSettings):
     input_token: SecretStr
     input_latest_changes_file: Path = Path("README.md")
     input_latest_changes_header: str = "### Latest Changes"
-    input_next_release_start: str = "^### "
     input_template_file: Path = Path(__file__).parent / "latest-changes.jinja2"
+    input_end_regex: str = "^### "
     input_debug_logs: Optional[bool] = False
     input_labels: List[Section] = [
         Section(label="breaking", header="#### Breaking Changes"),
@@ -96,7 +96,7 @@ def generate_content(
     pre_header_content = content[: header_match.end()].strip()
     post_header_content = content[header_match.end() :].strip()
     next_release_match = re.search(
-        settings.input_next_release_start, post_header_content, flags=re.MULTILINE
+        settings.input_end_regex, post_header_content, flags=re.MULTILINE
     )
     release_end = (
         len(content)
